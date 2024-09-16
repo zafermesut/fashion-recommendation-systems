ffbfc510554a8638aab6b78b7c331513986291c4
Zafer Mesut Bilen <wildones.zafer@gmail.com>
2024-09-16 17:49:27 +0300

    wip

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 import streamlit as st
-from sklearn.neighbors import NearestNeighbors
 import pickle
-from PIL import Image
-import os
+from sklearn.neighbors import NearestNeighbors
 
-image_features = pickle.load(open('Images_features.pkl', 'rb'))
-filenames = pickle.load(open('filenames.pkl', 'rb'))
+with open('Images_features.pkl', 'rb') as f:
+    image_features = pickle.load(f)
 
-
+with open('filenames.pkl', 'rb') as f:
+    filenames = pickle.load(f)
 
 model = NearestNeighbors(n_neighbors=5, algorithm='brute', metric='euclidean')
 model.fit(image_features)
-
 
 st.title("Fashion Product Recommender")
 
@@ -21,7 +19,7 @@ selected_index = filenames.index(selected_image)
 
 distances, indices = model.kneighbors([image_features[selected_index]])
 
+
 st.write("Here are similar products:")
 for idx in indices[0]:
-    st.image(filenames[idx], caption=filenames[idx])
-
+    st.write(filenames[idx])
